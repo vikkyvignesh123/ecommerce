b12c24afaedc10ba9a752ba53b5a079425b90fee
vikkyvignesh123 <vigneshsubramaniyan753@gmail.com>
2025-06-19 05:56:01 -0700

    changes

--- ecommercejs/script.js
+++ ecommercejs/script.js
@@ -1,5 +1,24 @@
-function goToProductPage(productdetails) 
-{
-        window.location.href = productdetails;
-    }
-    
+function goToProductPage(productdetails) {
+  window.location.href = productdetails;
+}
+let cartCount = 0;
+
+function toggleCart(element) {
+  // const cartIcon = document.getElementById("cart");
+  const cartCountElement = document.getElementById("cart-count");
+
+  // Toggle active class to track state
+  if (!element.classList.contains("active")) {
+    cartCount++;
+    element.classList.add("active");
+    // Optional: change heart color when added
+    element.style.color = "red";
+  } else {
+    cartCount--;
+    element.classList.remove("active");
+    element.style.color = "black"; // Optional: reset heart color when removed
+  }
+
+  cartCountElement.innerText = cartCount;
+}
+
